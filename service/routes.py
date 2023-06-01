365e2cc6215036f65b870ce57d5cbba0db525115
qkoo0833 <qkoo0833@gmail.com>
2023-06-01 06:09:21 -0400

    delete api for account

--- service/routes.py
+++ service/routes.py
@@ -101,6 +101,14 @@ def update_account(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
